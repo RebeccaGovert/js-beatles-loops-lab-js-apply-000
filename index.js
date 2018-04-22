a2117036ca20bfe796c28df696101a987c1a73e0
Rebecca Govert <becci.govert@gmail.com>
2018-04-22 17:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
+  var myarray = [];
   for (var i=0; i < musicians.length; i++) {
-    array.push(musicians[i] + ' plays ' + instruments[i]);
+    myarray.push(musicians[i] + ' plays ' + instruments[i]);
       }
-      return array;
+      return myarray;
 }
 
 const facts = [
