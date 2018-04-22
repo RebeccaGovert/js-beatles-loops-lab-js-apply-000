5e9c675b1020eb5ff674ddd40c0ff90b4aba44fa
Rebecca Govert <becci.govert@gmail.com>
2018-04-22 17:07:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i=0; i < musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+      }
+      return array;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  
+  function johnLennonFacts(facts) {
+    var results = [];
+    var count = 0;
+    while (count < facts.length) {
+      results.push(facts[count] + "!!!");
+      count++;
+    }
+    return results;
+  }
+
+function iLoveTheBeatles() {
+  
+}
